21a2bedf202affeb77a5e11d4140fa777218c9d2
Volodymyr <Havryshko.c.s@gmail.com>
2016-02-13 23:42:26 +0200

    added some minor changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160213140733) do
+ActiveRecord::Schema.define(version: 20160213144805) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "f_name",           limit: 255
@@ -34,9 +34,9 @@ ActiveRecord::Schema.define(version: 20160213140733) do
     t.text     "description", limit: 65535
     t.string   "place",       limit: 255
     t.datetime "date"
-    t.integer  "user_id",     limit: 4
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.integer  "user_id",     limit: 4
   end
 
   create_table "speeches", force: :cascade do |t|
@@ -65,7 +65,13 @@ ActiveRecord::Schema.define(version: 20160213140733) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "f_name",                 limit: 255
+    t.string   "l_name",                 limit: 255
+    t.string   "phone",                  limit: 255
+    t.string   "facebook_account",       limit: 255
+    t.string   "skype_account",          limit: 255
     t.integer  "city_id",                limit: 4
+    t.string   "login",                  limit: 255
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
     t.string   "reset_password_token",   limit: 255
