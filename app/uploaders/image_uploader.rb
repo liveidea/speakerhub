6f0a812e358dcfb5fff12ab3ae0a428d96b1c81c
Volodymyr <Havryshko.c.s@gmail.com>
2016-02-20 15:54:44 +0200

    Added speakers index page

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -17,12 +17,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url
+    # For Rails 3.1+ asset pipeline compatibility:
+    ActionController::Base.helpers.asset_path("assets/" + [version_name, "default.jpg"].compact.join('_'))
+
+    # "/assets/default.jpg" + [version_name, "default.jpg"].compact.join('_')
+  end
 
   # Process files as they are uploaded:
   #process :scale => [200, 300]
@@ -38,6 +38,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   version :show do
      process :resize_to_fit => [300, 300]
    end
+  version :show_user do
+    process :resize_to_fit => [100,100]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -56,4 +56,6 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryGirl::Syntax::Methods
   config.include RSpecHtmlMatchers
+  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::TestHelpers, type: :view
 end
