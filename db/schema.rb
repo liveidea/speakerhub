ebab94f07ddd0e6e52d91a8b7e68b1077c690a1a
freevova <freevova@ukr.net>
2016-03-04 21:24:47 +0200

    add some js to reqest_creation

--- db/schema.rb
+++ db/schema.rb
@@ -89,7 +89,6 @@ ActiveRecord::Schema.define(version: 20160302183610) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "login",                  limit: 255
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
     t.string   "reset_password_token",   limit: 255
